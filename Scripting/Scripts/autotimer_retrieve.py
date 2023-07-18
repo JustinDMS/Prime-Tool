4628c5dde3b8f6cb955e03ca890f8416f3f84fe4
JustinDMS <90155396+JustinDMS@users.noreply.github.com>
2023-07-18 13:55:03 -0500

    Updated retrieve script
    - Fixed address for speed
    - Updated output formatting to be easier to work with

--- Scripting/Scripts/autotimer_retrieve.py
+++ Scripting/Scripts/autotimer_retrieve.py
@@ -2,13 +2,14 @@ import dolphin_memory_engine as dme
 
 addresses : dict = {
     "Last Room Time": 0x80600978,
-    "Speed": 0x80600954,
+    "Speed": 0x8046BE78,
 }
 
 def main():
     dme.hook()
     if dme.is_hooked():
-       print(dme.read_double(addresses["Last Room Time"]), end='')
+       print(dme.read_double(addresses["Last Room Time"]), end=',')
+       print(dme.read_float(addresses["Speed"]), end='')
     else:
         print("Not Hooked", end='')
 
